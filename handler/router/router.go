0edeed672700b65b33c19585890df56ca7120c7d
OGI <65173657+togishima@users.noreply.github.com>
2025-02-02 00:16:20 +0900

    エンドポイント追加

--- handler/router/router.go
+++ handler/router/router.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/TechBowl-japan/go-stations/handler"
+	"github.com/TechBowl-japan/go-stations/service"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
@@ -13,6 +14,11 @@ func NewRouter(todoDB *sql.DB) *http.ServeMux {
 
 	// /healthz エンドポイントの登録
 	mux.Handle("/healthz", handler.NewHealthzHandler())
+
+	// /todos エンドポイントの登録
+	todoService := service.NewTODOService(todoDB)
+	todoHandler := handler.NewTODOHandler(todoService)
+	mux.Handle("/todos", todoHandler)
 
 	return mux
 }
