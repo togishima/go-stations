b8cfce45b3b3e765951b1b934fc5ec48774fcb19
OGI <65173657+togishima@users.noreply.github.com>
2025-02-01 23:29:16 +0900

    add: エンドポイント追加

--- handler/router/router.go
+++ handler/router/router.go
@@ -3,10 +3,16 @@ package router
 import (
 	"database/sql"
 	"net/http"
+
+	"github.com/TechBowl-japan/go-stations/handler"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	// register routes
 	mux := http.NewServeMux()
+
+	// /healthz エンドポイントの登録
+	mux.Handle("/healthz", handler.NewHealthzHandler())
+
 	return mux
 }
